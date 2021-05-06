130b4a873faf41cdf81b26e836191d280524d50e
Harikrishna Patnala <harikrishna.patnala@gmail.com>
2021-05-06 14:57:03 +0530

    Decoupled service offering and disk offering

--- server/src/main/java/com/cloud/api/query/QueryManagerImpl.java
+++ server/src/main/java/com/cloud/api/query/QueryManagerImpl.java
@@ -213,10 +213,12 @@ import com.cloud.storage.Storage;
 import com.cloud.storage.Storage.ImageFormat;
 import com.cloud.storage.Storage.TemplateType;
 import com.cloud.storage.StoragePoolTagVO;
+import com.cloud.storage.DiskOfferingVO;
 import com.cloud.storage.VMTemplateVO;
 import com.cloud.storage.Volume;
 import com.cloud.storage.dao.StoragePoolTagsDao;
 import com.cloud.storage.dao.VMTemplateDao;
+import com.cloud.storage.dao.DiskOfferingDao;
 import com.cloud.storage.dao.VMTemplateDetailsDao;
 import com.cloud.tags.ResourceTagVO;
 import com.cloud.tags.dao.ResourceTagDao;
@@ -356,6 +358,9 @@ public class QueryManagerImpl extends MutualExclusiveIdsManagerBase implements Q
 
     @Inject
     private ServiceOfferingDao _srvOfferingDao;
+
+    @Inject
+    private DiskOfferingDao _diskOfferingDao;
 
     @Inject
     private DataCenterJoinDao _dcJoinDao;
@@ -2926,7 +2931,8 @@ public class QueryManagerImpl extends MutualExclusiveIdsManagerBase implements Q
         if (currentVmOffering == null) {
             return offerings;
         }
-        List<String> currentTagsList = StringUtils.csvTagsToList(currentVmOffering.getTags());
+        DiskOfferingVO diskOfferingVO = _diskOfferingDao.findById(currentVmOffering.getDiskOfferingId());
+        List<String> currentTagsList = StringUtils.csvTagsToList(diskOfferingVO.getTags());
 
         // New service offering should have all the tags of the current service offering.
         List<ServiceOfferingJoinVO> filteredOfferings = new ArrayList<>();
