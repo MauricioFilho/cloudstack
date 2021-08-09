1a8e74e4fb503c6ca6291d7c098b0b77d65cb8b7
Harikrishna Patnala <harikrishna.patnala@gmail.com>
2021-08-09 09:18:11 +0530

    Addressed review comments

--- server/src/main/java/com/cloud/configuration/ConfigurationManagerImpl.java
+++ server/src/main/java/com/cloud/configuration/ConfigurationManagerImpl.java
@@ -2559,7 +2559,7 @@ public class ConfigurationManagerImpl extends ManagerBase implements Configurati
         tags = StringUtils.cleanupTags(tags);
 
         ServiceOfferingVO serviceOffering = new ServiceOfferingVO(name, cpu, ramSize, speed, networkRate, null, offerHA,
-                limitResourceUse, volatileVm, displayText, typedProvisioningType, localStorageRequired, false, tags, isSystem, vmType,
+                limitResourceUse, volatileVm, displayText, isSystem, vmType,
                 hostTag, deploymentPlanner, dynamicScalingEnabled);
 
         List<ServiceOfferingDetailsVO> detailsVO = new ArrayList<ServiceOfferingDetailsVO>();
@@ -2634,7 +2634,7 @@ public class ConfigurationManagerImpl extends ManagerBase implements Configurati
                     detailsVO.add(new ServiceOfferingDetailsVO(serviceOffering.getId(), ApiConstants.ZONE_ID, String.valueOf(zoneId), false));
                 }
             }
-            if (!detailsVO.isEmpty()) {
+            if (CollectionUtils.isNotEmpty(detailsVO)) {
                 for (ServiceOfferingDetailsVO detail : detailsVO) {
                     detail.setResourceId(serviceOffering.getId());
                 }
